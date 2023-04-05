003eb615d14115bbc3d8a6236e9f8d793ec45ec6
xandokay <82251429+xandokay@users.noreply.github.com>
2023-04-05 03:12:30 -0400

    initial

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var sound = new Howl({
+    src: ['backgroundmusic.mp3'],
+    loop: true,
+    volume: 0.1
+});
+
+function playSound() {
+    sound.play();
+    document.removeEventListener('click', playSound);
+}
+
+document.addEventListener('click', playSound);
+
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+
+function loaded(){
+
+	setInterval(loop, 350);
+
+}
+
+var x = 0;
+
+var titleText = [ "🖤 soft", "🖤 sof", "🖤 so", "🖤 s", "🖤 so", "🖤 sof", "🖤 soft"];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
